b19f8bd6a773fb4d24ef9b82d874f0c4b9370128
jimmy <lettevoueto.jimmy@gmail.com>
2025-07-03 09:30:03 +0200

    remove node_module and dist from git

--- backend/dist/src/documents/dto/create-document.input.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class CreateDocumentInput {
-    title: string;
-    content: string;
-    fileName?: string;
-    filePath?: string;
-    fileSize?: number;
-    mimeType?: string;
-    userId?: string;
-}
--- backend/dist/src/documents/dto/update-document.input.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class UpdateDocumentInput {
-    id: string;
-    title?: string;
-    content?: string;
-}
